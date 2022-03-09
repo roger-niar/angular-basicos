7ca9105ec0d459b2f57e67849cdfb4af5d1d4d21
macbook <macbook@RogerMBP.local>
2022-03-08 22:14:51 -0400

    primer commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+})
+export class ListadoComponent  {
+
+  heroes: string[] = ['Ironman', 'Hulk', 'Spiderman', 'Thor', 'Capitan America'];
+  //heroeBorrado: string = ' ';
+  heroeBorrado: string = '';
+
+  borrarHeroe(){
+    this.heroeBorrado = this.heroes.shift() || '';
+  }
+
+}
